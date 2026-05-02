01d5b506c5ba075ce068901e93872f3c610fb06d
Nicholas Lawrence <neeko223@gmail.com>
2026-05-01 22:55:47 -0400

    fixed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect('mongodb://127.0.0.1:27017/wtwr_db')
   .then(() => {
-    console.log("Connected to DB");
+    // console.log("Connected to DB");
   })
   .catch(console.error);
 
@@ -25,5 +25,5 @@ app.use((req, res, next) => {
 app.use("/", mainRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  // console.log(`Server is running on port ${PORT}`);
 });
