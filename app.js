90b12ae8fd5e779b78bff353cc28a4acc384c43d
Nicholas Lawrence <neeko223@gmail.com>
2026-05-17 22:59:55 -0400

    change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mainRouter = require("./routes/index");
 const auth = require("./middlewares/auth");
 
 const { login, createUser } = require("./controllers/users");
-const { getItems } = require("./controllers/clothingItems");
+const { getItems } = require("./controllers/clothingItems.js");
 
 const app = express();
 
