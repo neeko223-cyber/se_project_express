ba437f98d5f39dd5fc8cff063607d1c5d960f309
Nicholas Lawrence <neeko223@gmail.com>
2026-05-20 22:48:56 -0400

    changes

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user.js");
+const User = require("../models/user");
 const {
   BAD_REQUEST,
   NOT_FOUND,
