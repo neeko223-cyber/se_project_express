e73b5c942cfe6a4a59e57593381a880fc8c21a11
Nicholas Lawrence <neeko223@gmail.com>
2026-06-02 22:29:33 -0400

    Changed

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 
 const mainRouter = require("./routes/index");
 
-const auth = require("./middlewares/auth");
-
 const { login, createUser } = require("./controllers/users");
 const { getItems, createItem, deleteItem, likeItem, dislikeItem, } = require("./controllers/clothingitems");
 
