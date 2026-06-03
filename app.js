608bb8cf18edb376017ae48fba87e439010e8928
Nicholas Lawrence <neeko223@gmail.com>
2026-06-02 22:00:48 -0400

    update

--- app.js
+++ app.js
@@ -30,7 +30,13 @@ app.post("/signup", createUser);
 
 app.get("/items", getItems);
 
-app.use(auth);
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+
+  next();
+});
 
 app.post("/items", createItem);
 app.put("/items/:itemId/likes", likeItem);
