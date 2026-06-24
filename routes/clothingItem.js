b267f4ba4d4e6d5ff74e559300673054ff8708ec
Nicholas Lawrence <neeko223@gmail.com>
2026-06-24 19:21:01 -0400

    check

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 
-const { getItems, createItem, likeItem, dislikeItem, deleteItem } = require('../controllers/clothingitems');
+const {
+  getItems,
+  createItem,
+  likeItem,
+  dislikeItem,
+  deleteItem
+} = require('../controllers/clothingItems');
 
-router.get('/items', getItems);
-router.post('/items', createItem);
-router.put('/items/:itemId/likes', likeItem);
-router.delete('/items/:itemId/likes', dislikeItem);
-router.delete('/items/:itemId', deleteItem);
+router.get("/", getItems);
+router.post("/", createItem);
+router.put("/:itemId/likes", likeItem);
+router.delete("/:itemId/likes", dislikeItem);
+router.delete("/:itemId", deleteItem);
 
 
 module.exports = router;
