39d84727cca8974dba96d6400663986d15d75d5a
Nicholas Lawrence <neeko223@gmail.com>
2026-06-23 22:15:16 -0400

    Items

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const { getItems, createItem, likeItem, dislikeItem, deleteItem } = require('../controllers/clothingItems');
 
-router.get('/', getItems);
-router.post('/', createItem);
-router.put('/:itemId/likes', likeItem);
-router.delete('/:itemId/likes', dislikeItem);
-router.delete('/:itemId', deleteItem);
+router.get('/items', getItems);
+router.post('/items', createItem);
+router.put('/items/:itemId/likes', likeItem);
+router.delete('/items/:itemId/likes', dislikeItem);
+router.delete('/items/:itemId', deleteItem);
 
 
 module.exports = router;
