a9b62cd05d9a2b80490f0e3779f24c77030f2bba
Nicholas Lawrence <neeko223@gmail.com>
2026-07-20 22:33:47 -0400

    Uncomment console log statements for connection info

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect('mongodb://127.0.0.1:27017/wtwr_db')
   .then(() => {
-    // console.log("Connected to DB");
+    console.log("Connected to DB");
   })
   .catch(console.error);
 
@@ -22,5 +22,5 @@ app.use(express.json());
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {
-  // console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
